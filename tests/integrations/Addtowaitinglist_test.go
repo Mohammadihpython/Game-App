65061ce0d898afba2eea7ef1d4e54094ebacf7d6
Mohammadihpython <mohammadih87@yahoo.com>
2025-05-16 17:43:56 +0330

    bug fix

--- tests/integrations/Addtowaitinglist_test.go
+++ tests/integrations/Addtowaitinglist_test.go
@@ -1,16 +1,13 @@
 package integrations
 
 import (
-	"GameApp/param"
 	"github.com/labstack/echo/v4"
 	"testing"
 )
 
 func TestAddToWaitingList(t *testing.T) {
 	e := echo.New()
-	reqBody := param.AddToWaitingListRequest{
-		UserID:   "",
-		Category: "",
-	}
+
+	println(e)
 
 }
